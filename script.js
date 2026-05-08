78a7dfd44daa4131a1363140a965113cfcea9061
Rocio Argañaraz <arganaraz.rochii24@gmail.com>
2026-05-08 14:49:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
 function saludar() {
-    alert("Hola Rochi 👋 Bienvenida ");
+    alert("Hola Rochi 👋 Bienvenida");
 }
 
 function despedir() {
-    alert("Chau Rochi, gracias por pasar por mi pagina ");
+    alert("Chau Rochi, gracias por pasar por mi página");
 }
 
 function mostrarSeccion(id) {
 
-    /* BUSCA TODAS LAS SECCIONES */
     const secciones = document.querySelectorAll(".seccion");
 
-    /* OCULTA TODAS */
     secciones.forEach(seccion => {
         seccion.style.display = "none";
     });
 
-    /* MUESTRA SOLO LA QUE CLICKEASTE */
     document.getElementById(id).style.display = "block";
 }
 
-/* SECCION QUE SE VE AL ABRIR LA PAGINA */
+/* PAGINA INICIAL */
 
-mostrarSeccion('inicio');
+mostrarSeccion("inicio");
