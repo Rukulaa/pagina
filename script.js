2a2bffd16ed76fc9a7aa0f9790fac85303210f3c
Rocio Argañaraz <arganaraz.rochii24@gmail.com>
2026-05-08 14:04:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function saludar() {
     alert("Hola Rochi 👋 Bienvenida ");
 }
+
 function despedir() {
     alert("Chau Rochi, gracias por pasar por mi pagina ");
+}
+
+function mostrarSeccion(id) {
+    const secciones = document.querySelectorAll(".seccion");
+
+    secciones.forEach(seccion => {
+        seccion.style.display = "none";
+    });
+
+    document.getElementById(id).style.display = "block";
 }
